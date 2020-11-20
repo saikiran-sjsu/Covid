e343bb99b14de3bcafd2fe4a08eebffc1ebbaa98
Elias Faris <eliasfariss@gmail.com>
2020-11-19 23:00:47 -0800

    awsRDS & mySQL connection to django & covid API integration

--- covid/covid/settings.py
+++ covid/covid/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'covid.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'covid',
+        'USER': 'root',
+        'PASSWORD': 'password123',
+        'HOST': 'covid.c8idaxlxroso.us-west-1.rds.amazonaws.com',
+        'PORT': '3306',
+
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- covid/frontend/src/components/App.js
+++ covid/frontend/src/components/App.js
@@ -2,13 +2,51 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 
 export default class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
+
+        }
     }
 
-        render() {
-            return (<h1>Testing React Code</h1>);
+    componentDidMount() {
+
+        fetch('https://api.covidtracking.com/v1/states/current.json')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    items: json,
+                })
+            });
+
+    }
+
+    render() {
+
+        var { isLoaded, items } = this.state;
+        if (!isLoaded) {
+            return <div>Loading...</div>;
         }
+        else {
+            return (
+                <div className="App">
+                   <ul>
+                      {items.map(item => (
+                        <li key = {item.date}>
+                            State : {item.state} | Positive Cases: {item.positive} | Negative cases: {item.negative} | 
+                        </li>
+                      ))}; 
+                   </ul>
+
+
+                </div>
+            );
+        }
+
+    }
 }
 
 const appDiv = document.getElementById("app");
