98ebdabb0cef84d924a5f16fbe5d22a2fa1de842
renadmorrar <47757618+renadmorrar@users.noreply.github.com>
2020-12-03 23:17:45 -0800

    Update Testing.js

--- covid/frontend/src/components/Testing.js
+++ covid/frontend/src/components/Testing.js
@@ -7,38 +7,34 @@ export default class Testing extends Component {
     }
     render() {
         document.title = 'Covid | Testing'
-        return(
+        return (
             <div>
                 <center>
-                <br></br>
-                <h1>Testing Websites</h1>
-                <br></br>
-                <br></br>
-                <a class="btn btn-link" href="https://www.hhs.gov/coronavirus/community-based-testing-sites/index.html" target="_blank">Health and Human Services</a>
-                <br></br>
-                <br></br>
-                <a class="btn btn-link" href="https://www.anthem.com/microsites/covid19/test-site-finder/" target="_blank">Anthem</a>
-                <br></br>
-                <br></br>
-                <a class="btn btn-link" href="https://my.castlighthealth.com/corona-virus-testing-sites/" target="_blank">Castlight</a>
-                <br></br>
-                <br></br>
-                <a class="btn btn-link" href="https://www.sccgov.org/sites/covid19/Pages/press-release-11-6-2020-daily-covid19-tests-rise-1000-per-day.aspx" target="_blank">SCC Public Health</a>
-                <br></br>
-                <br></br>
-                <a class="btn btn-link" href="https://covid19.lacounty.gov/testing/" target="_blank">County of LA</a>
-                
+                    <h1>Testing Websites</h1>
+                    <br></br>
                 </center>
-                
-
+                <center>
+                    <table border='3px'>
+                        <tr>
+                            <a class="btn btn-link" href="https://www.hhs.gov/coronavirus/community-based-testing-sites/index.html" target="_blank">Health and Human Services</a>
+                            <br></br>
+                            <br></br>
+                            <a class="btn btn-link" href="https://www.anthem.com/microsites/covid19/test-site-finder/" target="_blank">Anthem</a>
+                            <br></br>
+                            <br></br>
+                        </tr>
+                        <tr>
+                            <a class="btn btn-link" href="https://my.castlighthealth.com/corona-virus-testing-sites/" target="_blank">Castlight</a>
+                            <br></br>
+                            <br></br>
+                            <a class="btn btn-link" href="https://www.sccgov.org/sites/covid19/Pages/press-release-11-6-2020-daily-covid19-tests-rise-1000-per-day.aspx" target="_blank">SCC Public Health</a>
+                            <br></br>
+                            <br></br>
+                            <a class="btn btn-link" href="https://covid19.lacounty.gov/testing/" target="_blank">County of LA</a>
+                        </tr>
+                    </table>
+                </center>
             </div>
-
-
-                
-        
         );
-
-        
-        
     }
 }
